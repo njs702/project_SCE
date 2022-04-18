adb603933b98416f8f9bd85e0dcecaa71973b886
junseok <njs702@ajou.ac.kr>
2022-04-18 11:04:36 +0900

    Use the raspberry pi camera to film and save videos

--- opencv_project/prevent_drowsy_drive.py
+++ opencv_project/prevent_drowsy_drive.py
@@ -2,14 +2,28 @@ import cv2
 
 cap = cv2.VideoCapture(0) # default camera
 
+# height, width setting
+width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+print("size: {0} x {1}".format(width,height))
+
+# 영상 저장을 위한 VideoWriter
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+writer = cv2.VideoWriter('test.avi',fourcc,24,(int(width),int(height)))
+
 while cap.isOpened():
     # 카메라 프레임 읽기
     success, frame = cap.read()
     if success:
-        cv2.imshow('Camera Window',frame)
+        writer.write(frame) # 프레임 저장
+        cv2.imshow('Video Window',frame)
 
-        key = cv2.waitKey(1) & 0xFF
+        key = cv2.waitKey(1) & 0xFF == ord('q')
         if key == 27:
             break
+    else:
+        break
 
 cap.release()
+writer.release()
+cv2.destroyAllWindows()
