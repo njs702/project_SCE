8fe59fe7e1d6b200322c30fc3ee0c8ca50774931
junseok <njs702@ajou.ac.kr>
2022-04-18 16:29:14 +0900

    음악 플레이어 백그라운드 종료 구현

--- music_player/stop_music.py
+++ music_player/stop_music.py
@@ -1,2 +1,16 @@
 import pygame
 import os
+
+# pid를 읽어올 파일 path를 저장한다
+file_path = "/home/pi/id_saver.txt"
+
+# read 모드로 파일을 열고 적혀 있는 pid를 읽는다
+f = open(file_path, "r")
+s = f.readline()
+
+# 해당 process id에 맞는 프로세스를 kill(노래 종료)
+os.system("fg")
+os.system("kill " + s)
+
+# 해당 id_saver 파일 제거
+os.system("rm -rf " + file_path)
