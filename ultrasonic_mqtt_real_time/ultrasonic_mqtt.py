8d2742dd25814b2f7b85510e1dce5ba8d3bdbf1c
junseok <njs702@ajou.ac.kr>
2022-04-22 10:40:39 +0900

    함수 적용 및 브로커에 연결

--- ultrasonic_mqtt_real_time/ultrasonic_mqtt.py
+++ ultrasonic_mqtt_real_time/ultrasonic_mqtt.py
@@ -19,3 +19,27 @@ def on_publish(client, userdata, mid):
 # subscribe from nodeMCU - info = real_time_distance(from HC-SR04)
 def on_subscribe(client, userdata, mid, granted_qos):
     print("subscribed: " + str(mid) + " " + str(granted_qos))
+
+# create new client
+client = mqtt.Client()
+
+# set callback function
+# on_connect - connect to broker
+# on_disconnect - disconnect from broker
+# on_subscribe & on_publish - subscribe & publish
+# on_message - accept message using subscribe from publisher
+
+client.on_connect = on_connect
+client.on_disconnect = on_disconnect
+client.on_subscribe = on_subscribe
+client.on_message = on_message
+client.on_publish = on_publish
+
+# address : localhost, port: 1883 에 연결
+client.connect('192.168.0.80', 1883)
+
+# outTopic topic 구독해서 메세지 받아옴
+client.subscribe('outTopic', 1)
+
+# loop until message arrive
+client.loop_forever()
