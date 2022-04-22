bc038197928fe1ae7f0722e5f189356229f82d1c
ming <ming2686@naver.com>
2022-04-22 11:57:45 +0900

    setup_wife()함수추가

--- accident_location_mqtt/accident_loaction.ino
+++ accident_location_mqtt/accident_loaction.ino
@@ -53,3 +53,19 @@ PubSubClient client(espClient);
 long lastMsg = 0;
 int value = 0;
 
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  randomSeed(micros());
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
