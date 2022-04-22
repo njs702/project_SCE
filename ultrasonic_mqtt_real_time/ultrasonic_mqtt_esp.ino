89c0f1e3fd5451f71bc77d3d104d5d0ea9d99821
junseok <njs702@ajou.ac.kr>
2022-04-22 10:52:22 +0900

    subscribe callback 함수 작성

--- ultrasonic_mqtt_real_time/ultrasonic_mqtt_esp.ino
+++ ultrasonic_mqtt_real_time/ultrasonic_mqtt_esp.ino
@@ -61,3 +61,23 @@ void setup_wifi() {
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 }
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is acive low on the ESP-01)
+  } else {
+    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+  }
+
+}
