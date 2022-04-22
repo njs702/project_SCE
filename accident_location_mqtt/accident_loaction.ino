c43197ca9e86aa9fd0e5e316561487f48171d03f
ming <ming2686@naver.com>
2022-04-22 12:01:16 +0900

    callback함수추가

--- accident_location_mqtt/accident_loaction.ino
+++ accident_location_mqtt/accident_loaction.ino
@@ -69,3 +69,13 @@ void setup_wifi() {
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 }
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
