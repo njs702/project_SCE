0c85956fef57e1d32ba7eb0cbdbe2e3820cd80de
ming <ming2686@naver.com>
2022-04-22 12:05:56 +0900

    reconnect()함수추가

--- accident_location_mqtt/accident_loaction.ino
+++ accident_location_mqtt/accident_loaction.ino
@@ -79,3 +79,26 @@ void callback(char* topic, byte* payload, unsigned int length) {
   }
   Serial.println();
 }
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("ESP8266/GPS", "hello world");
+      // ... and resubscribe
+      client.subscribe("inTopic");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
