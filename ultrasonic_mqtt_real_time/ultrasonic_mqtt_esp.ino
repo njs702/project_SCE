55cafaa8118287e1371230d6cd9f2eb4d14dc0ac
junseok <njs702@ajou.ac.kr>
2022-04-22 10:51:22 +0900

    설정한 네트워크 정보를 사용해 wifi setup

--- ultrasonic_mqtt_real_time/ultrasonic_mqtt_esp.ino
+++ ultrasonic_mqtt_real_time/ultrasonic_mqtt_esp.ino
@@ -40,3 +40,24 @@ float readUltraSonic(){
     howlong = ((float)(340*return_time) / 10000)/2;
     return howlong;
 }
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
