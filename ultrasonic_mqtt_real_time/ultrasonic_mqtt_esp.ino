acaea8493fd79285fcdc30f5030c50e7f8f91a5e
junseok <njs702@ajou.ac.kr>
2022-04-22 10:50:40 +0900

    초음파 센서 trig, echo 사용해 거리 측정하는 함수 작성

--- ultrasonic_mqtt_real_time/ultrasonic_mqtt_esp.ino
+++ ultrasonic_mqtt_real_time/ultrasonic_mqtt_esp.ino
@@ -28,3 +28,15 @@ void setup() {
 
   // digitalWrite(BUILTIN_LED,LOW);
 }
+
+float readUltraSonic(){
+    float return_time, howlong;
+    // 초음파 센서는 초음파 발사 후 돌아오는 시간을 역산하여 거리 계산
+    digitalWrite(trig,HIGH); // 초음파 발사
+    delay(5);
+    digitalWrite(trig,LOW); // 초음파 정시
+    return_time = pulseIn(echo,HIGH); // 돌아오는 시간
+    // 시간을 거리로 계산
+    howlong = ((float)(340*return_time) / 10000)/2;
+    return howlong;
+}
